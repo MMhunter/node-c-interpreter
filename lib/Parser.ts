0fd67b2c540ef622772b40d7cd5f53102dc95116
mmhunter <mmhuntermax@gmail.com>
2017-07-10 19:20:29 +0800

    Parser start

---
+++ lib/Parser.ts
@@ -0,0 +1,27 @@
+import {Token} from "node-c-interpreter/lib/Lexer";
+/**
+ * @file Parser.class.js
+ *
+ * Created by mmhunter on 09/07/2017.
+ */
+
+export class Parser {
+
+    private tokens: Token[] = [];
+
+    private index: number = -1;
+
+    constructor(tokens: Token[]) {
+
+    }
+
+    private program() {
+
+    }
+
+    private nextToken(){
+
+    }
+
+}
+
