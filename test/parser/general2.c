a4f06174963db7e1b04c41d8406b4ed191c424f7
mmhunter <mmhuntermax@gmail.com>
2017-07-25 17:17:47 +0800

    fix lexer : error

--- test/parser/general2.c
+++ test/parser/general2.c
@@ -1,18 +1,6 @@
-struct Dot{
-    int x;
-    int y;
-};
-
-Dot createDot(int x, int y){
-    Dot dot = {0,0};
+void loop(){
+    switch (i) {
+        case 1:
+            break;
+    }
 }
-
-enum SHAPE_TYPE
-{
-      SQUARE=0
-};
-
-struct Shape{
-    Dot dots[4];
-    SHAPE_TYPE type;
-};
